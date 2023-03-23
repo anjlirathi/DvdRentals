a17695fac81ebaaf1675a5e83379c3521b817d0b
anjlirathi <anjalirathi987@gmail.com>
2023-03-24 01:05:24 +0530

    ACtor Insights

--- market_analysis/complete_join_dataset.sql
+++ market_analysis/complete_join_dataset.sql
@@ -697,3 +697,79 @@ SELECT * FROM category_recommendations
 WHERE customer_id =1
 ORDER BY 
   category_rank, reco_rank;
+
+  --ACTOR Insights
+
+DROP TABLE IF EXISTS actor_joint_dataset;
+CREATE TEMP TABLE actor_joint_dataset AS
+SELECT 
+  rental.customer_id,
+  rental.rental_id,
+  rental.rental_date,
+  film.film_id,
+  film.title,
+  actor.actor_id,
+  actor.first_name,
+  actor.last_name
+FROM dvd_rentals.rental
+INNER JOIN dvd_rentals.inventory
+ON rental.inventory_id = inventory.inventory_id
+INNER JOIN dvd_rentals.film
+ON inventory.film_id = film.film_id
+INNER JOIN dvd_rentals.film_actor
+ON film.film_id = film_actor.film_id
+INNER JOIN dvd_rentals.actor
+ON film_actor.actor_id= actor.actor_id;
+
+  --Output
+  SELECT * 
+  FROM actor_joint_dataset
+  LIMIT 10;
+
+
+----Top Actor counts
+
+DROP TABLE IF EXISTS top_actor_counts;
+CREATE TEMP TABLE top_actor_counts AS
+WITH actor_counts AS (
+  SELECT
+    customer_id,
+    actor_id,
+    first_name,
+    last_name,
+    COUNT(*) AS rental_count,
+    -- we also generate the latest_rental_date just like our category insight
+    MAX(rental_date) AS latest_rental_date
+  FROM actor_joint_dataset
+  GROUP BY
+    customer_id,
+    actor_id,
+    first_name,
+    last_name
+),
+ranked_actor_counts AS (
+  SELECT
+    actor_counts.*,
+    DENSE_RANK() OVER (
+      PARTITION BY customer_id
+      ORDER BY
+        rental_count DESC,
+        latest_rental_date DESC,
+        -- just in case we have any further ties, we'll throw in the names too!
+        first_name,
+        last_name
+    ) AS actor_rank
+  FROM actor_counts
+)
+SELECT
+  customer_id,
+  actor_id,
+  first_name,
+  last_name,
+  rental_count
+FROM ranked_actor_counts
+WHERE actor_rank = 1;
+
+--Output
+SELECT * FROM top_actor_counts;
+
